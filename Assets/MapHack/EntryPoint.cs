a832fa6e93610c5c1636cfefa2c5377e8b52c490
Hi-king <hikingko1@gmail.com>
2018-06-29 15:16:09 +0900

    :star: first moving map

---
+++ Assets/MapHack/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MapHack
+{
+    public class EntryPoint: MonoBehaviour
+    {
+        private WrldMap _wrldMap;
+
+        private void Start()
+        {
+//            _wrldMap = gameObject.AddComponent<WrldMap>();
+        }
+    }
+}
